b84e6f078b49ed287acf07840390899e5edb9300
minusLi <nmjbh@qq.com>
2022-08-13 11:19:50 +0800

    修正setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1.0',
     description='Random generation of Chinese place names',
     author='uint8_t',
+    url='https://github.com/uint8-t/chinese-address-generator',
     author_email='nmjbh@qq.com',
     packages=find_packages(),
     include_package_data=True,
